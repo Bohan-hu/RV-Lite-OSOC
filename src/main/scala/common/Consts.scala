7bddaa8eff5913f8786b61964cfa56ee80516d41
Bohan-Hu <bohan.hu@ieee.org>
2020-09-15 00:53:53 +0800

    Decode and constants on their ways

--- src/main/scala/common/Consts.scala
+++ src/main/scala/common/Consts.scala
@@ -1,23 +1,97 @@
-package common
+package common {
 
-import chisel3._
-import chisel3.util._
-import scala.math._
+  import chisel3._
+  import chisel3.util._
+  import scala.math._
 
-trait Consts {
-  // 用以译码的一些常量
-  val RD_MSB = 11
-  val RD_LSB = 7
-  val RS1_MSB = 19
-  val RS1_LSB = 15
-  val RS2_MSB = 24
-  val RS2_LSB = 20
+  // TODO: Use enum to replace the constants
+  // TODO: Use zip to replace the xx :: xx :: xx style assignment (Think of a list of signal)
+  object OpConstants {
 
-  val CSR_ADDR_MSB = 31
-  val CSR_ADDR_LSB = 20
+    //************************************
+    // Control Signals
+    val Y = true.B
+    val N = false.B
 
-  //    val NOP = 0x0013.U(32.W)  // NOP 指令
+    // PC Select Signal
+    val PC_4 = 0.asUInt(2.W) // PC + 4
+    val PC_BRJMP = 1.asUInt(2.W) // brjmp_target
+    val PC_JALR = 2.asUInt(2.W) // jump_reg_target
+    val PC_EXC = 3.asUInt(2.W) // exception
+
+    // Branch Type
+    val BR_N = 0.asUInt(4.W) // Next
+    val BR_NE = 1.asUInt(4.W) // Branch on NotEqual
+    val BR_EQ = 2.asUInt(4.W) // Branch on Equal
+    val BR_GE = 3.asUInt(4.W) // Branch on Greater/Equal
+    val BR_GEU = 4.asUInt(4.W) // Branch on Greater/Equal Unsigned
+    val BR_LT = 5.asUInt(4.W) // Branch on Less Than
+    val BR_LTU = 6.asUInt(4.W) // Branch on Less Than Unsigned
+    val BR_J = 7.asUInt(4.W) // Jump
+    val BR_JR = 8.asUInt(4.W) // Jump Register
+
+    // Operand #1 Select Signal
+    // Operand #1 could be RS1, PC.
+    val OP1_RS1 = 0.asUInt(2.W) // Register Source #1
+    val OP1_PC = 1.asUInt(2.W)  // PC
+    val OP1_X = 0.asUInt(2.W)   // Don't care
+
+    // Operand #2 Select Signal
+    // Operand #2 could be RS2, sign-extended or zero-extended(for CSRs) immediate
+    val OP2_RS2 = 0.asUInt(3.W) // Register Source #2
+    val IMM_ITYPE = 1.asUInt(3.W) // I-type
+    val IMM_STYPE = 2.asUInt(3.W) // S-type
+    val IMM_BTYPE = 3.asUInt(3.W) // B-type
+    val IMM_UTYPE = 4.asUInt(3.W) // U-type
+    val IMM_JTYPE = 5.asUInt(3.W) // J-type
+    val IMM_ZEXT = 6.asUInt(3.W) // Zero-extended Immediate from RS1 field, for use by CSRI instructions
+    val OP2_X = 0.asUInt(3.W)
+
+    // ALU Operation Signal
+    val ALU_ADD = 0.asUInt(4.W)
+    val ALU_SUB = 1.asUInt(4.W)
+    val ALU_SLL = 2.asUInt(4.W)
+    val ALU_SRL = 3.asUInt(4.W)
+    val ALU_SRA = 4.asUInt(4.W)
+    val ALU_AND = 5.asUInt(4.W)
+    val ALU_OR = 6.asUInt(4.W)
+    val ALU_XOR = 7.asUInt(4.W)
+    val ALU_SLT = 8.asUInt(4.W)
+    val ALU_SLTU = 9.asUInt(4.W)
+    val ALU_COPY_1 = 10.asUInt(4.W)
+    val ALU_COPY_2 = 11.asUInt(4.W)
+    val ALU_X = 0.asUInt(4.W)
+
+    // Writeback Select Signal
+    val WB_ALU = 0.asUInt(2.W)
+    val WB_MEM = 1.asUInt(2.W)
+    val WB_PC4 = 2.asUInt(2.W)
+    val WB_CSR = 3.asUInt(2.W)
+    val WB_X = 0.asUInt(2.W)
+
+    // Memory Write Signal
+    val MWR_0 = false.B
+    val MWR_1 = true.B
+    val MWR_X = false.B
+
+    // Memory Enable Signal
+    val MEN_0 = false.B
+    val MEN_1 = true.B
+    val MEN_X = false.B
+
+    // Mem Op
+    val MEM_NOP = 0.U
+    val MEM_READ = 1.U
+    val MEM_WRITE = 2.U
+
+    // Memory Mask Type Signal
+    val MSK_B = 0.asUInt(3.W)
+    val MSK_BU = 1.asUInt(3.W)
+    val MSK_H = 2.asUInt(3.W)
+    val MSK_HU = 3.asUInt(3.W)
+    val MSK_W = 4.asUInt(3.W)
+    val MSK_X = 4.asUInt(3.W)
+
+  }
 
 }
-
-
--- src/main/scala/core/decode.scala
+++ src/main/scala/core/decode.scala
@@ -1,7 +1,72 @@
 package core
-import defines._
-// See README.md for license details.
+import common.OpConstants._
+import common.Instructions._
 import chisel3._
+
+class Decode {
+  // Always put the imm at the position OP2
+  // TODO: Use a muxlookup and the constant definition and the array table to define the source operand
+//  val decodeops =
+//    Array(      /* val  |  BR  |  op1  |   op2     |  R1  |  R2  |    ALU     |  wb    | rf   | mem  |   mem     | mask  | csr | fence.i */
+//                /* inst | type |   sel |    sel    |  ren |  ren |     op     |  sel   | wen  |  en  |    wr     | type  | cmd |         */
+//      LW     -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_ADD    , WB_MEM ,  Y   ,  Y   , MEM_READ  , MT_W  , CSR.N, N),
+//      LB     -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_ADD    , WB_MEM ,  Y   ,  Y   , MEM_READ  , MT_B  , CSR.N, N),
+//      LBU    -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_ADD    , WB_MEM ,  Y   ,  Y   , MEM_READ  , MT_BU , CSR.N, N),
+//      LH     -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_ADD    , WB_MEM ,  Y   ,  Y   , MEM_READ  , MT_H  , CSR.N, N),
+//      LHU    -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_ADD    , WB_MEM ,  Y   ,  Y   , MEM_READ  , MT_HU , CSR.N, N),
+//      SW     -> List(Y, BR_N  , OP1_RS1, IMM_STYPE ,  Y   ,  Y   , ALU_ADD    , WB_X   ,  N   ,  Y   , MEM_WRITE , MT_W  , CSR.N, N),
+//      SB     -> List(Y, BR_N  , OP1_RS1, IMM_STYPE ,  Y   ,  Y   , ALU_ADD    , WB_X   ,  N   ,  Y   , MEM_WRITE , MT_B  , CSR.N, N),
+//      SH     -> List(Y, BR_N  , OP1_RS1, IMM_STYPE ,  Y   ,  Y   , ALU_ADD    , WB_X   ,  N   ,  Y   , MEM_WRITE , MT_H  , CSR.N, N),
+//      // IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+//      AUIPC  -> List(Y, BR_N  , OP1_PC , IMM_UTYPE ,  N   ,  N   , ALU_ADD    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      LUI    -> List(Y, BR_N  , OP1_X  , IMM_UTYPE ,  N   ,  N   , ALU_COPY_2 , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      ADDI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_ADD    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      ANDI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_AND    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      ORI    -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_OR     , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      XORI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_XOR    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SLTI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_SLT    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SLTIU  -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_SLTU   , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SLLI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_SLL    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SRAI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_SRA    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SRLI   -> List(Y, BR_N  , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_SRL    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      // RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+//      SLL    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_SLL    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      ADD    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_ADD    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SUB    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_SUB    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SLT    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_SLT    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SLTU   -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_SLTU   , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      AND    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_AND    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      OR     -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_OR     , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      XOR    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_XOR    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SRA    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_SRA    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      SRL    -> List(Y, BR_N  , OP1_RS1, OP2_RS2   ,  Y   ,  Y   , ALU_SRL    , WB_ALU ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      JAL    -> List(Y, BR_J  , OP1_RS1, IMM_JTYPE ,  N   ,  N   , ALU_X      , WB_PC4 ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      JALR   -> List(Y, BR_JR , OP1_RS1, IMM_ITYPE ,  Y   ,  N   , ALU_X      , WB_PC4 ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      BEQ    -> List(Y, BR_EQ , OP1_RS1, IMM_BTYPE ,  Y   ,  Y   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      BNE    -> List(Y, BR_NE , OP1_RS1, IMM_BTYPE ,  Y   ,  Y   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      BGE    -> List(Y, BR_GE , OP1_RS1, IMM_BTYPE ,  Y   ,  Y   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      BGEU   -> List(Y, BR_GEU, OP1_RS1, IMM_BTYPE ,  Y   ,  Y   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      BLT    -> List(Y, BR_LT , OP1_RS1, IMM_BTYPE ,  Y   ,  Y   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      BLTU   -> List(Y, BR_LTU, OP1_RS1, IMM_BTYPE ,  Y   ,  Y   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N),
+//      CSRRWI -> List(Y, BR_N  , OP1_X  , IMM_ZEXT  ,  Y   ,  Y   , ALU_COPY_2 , WB_CSR ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.W, N),
+//      CSRRSI -> List(Y, BR_N  , OP1_X  , IMM_ZEXT  ,  Y   ,  Y   , ALU_COPY_2 , WB_CSR ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.S, N),
+//      CSRRW  -> List(Y, BR_N  , OP1_RS1, OP2_X     ,  Y   ,  Y   , ALU_COPY_1 , WB_CSR ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.W, N),
+//      CSRRS  -> List(Y, BR_N  , OP1_RS1, OP2_X     ,  Y   ,  Y   , ALU_COPY_1 , WB_CSR ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.S, N),
+//      CSRRC  -> List(Y, BR_N  , OP1_RS1, OP2_X     ,  Y   ,  Y   , ALU_COPY_1 , WB_CSR ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.C, N),
+//      CSRRCI -> List(Y, BR_N  , OP1_X  , IMM_ZEXT  ,  Y   ,  Y   , ALU_COPY_2 , WB_CSR ,  Y   ,  N   , MEM_NOP   , MT_X  , CSR.C, N),
+//      ECALL  -> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.I, N),
+//      MRET   -> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.I, N),
+//      URET   -> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.I, N),
+//      EBREAK -> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.I, N),
+//      WFI    -> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, N), // implemented as a NOP
+//
+//      FENCE_I-> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  N   , MEM_NOP   , MT_X  , CSR.N, Y),
+//      // kill pipeline and refetch instructions since the pipeline will be holding stall instructions.
+//      FENCE  -> List(Y, BR_N  , OP1_X  , OP2_X     ,  N   ,  N   , ALU_X      , WB_X   ,  N   ,  Y   , MEM_NOP   , MT_X  , CSR.N, N)
+//      // we are already sequentially consistent, so no need to honor the fence instruction
+//  )
+}
+
 //class decode extends Module with defines.decodeConsts {
 //  val io = IO(new Bundle {
 //    val ifetch = new defines.IFetch
