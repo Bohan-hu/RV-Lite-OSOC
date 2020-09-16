6008c96ab560b2c282260ea10b05eb525d1c6367
Bohan-Hu <bohan.hu@ieee.org>
2020-09-16 22:21:23 +0800

    ALU simpler

--- src/main/scala/core/alu.scala
+++ src/main/scala/core/alu.scala
@@ -3,17 +3,12 @@ package core
 import chisel3._
 import common.util.signExt64
 import chisel3.util._
-
-trait ALUConsts {
-  val alu_ADD :: alu_SUB :: alu_AND :: alu_OR :: alu_XOR :: alu_SLT :: alu_SLL :: alu_SLTU :: alu_SRL :: alu_SRA :: alu_A :: alu_B :: Nil = Enum(12)
-  val op_Arith :: op_Compare :: op_Shift :: op_Logic :: op_Copy :: Nil = Enum(5)
-}
+import common.OpConstants._
 
 class ALUIO(srcWidth: Int) extends Bundle with ConfigParams {
   val srcA = Input(UInt(srcWidth.W))
   val srcB = Input(UInt(srcWidth.W))
   val aluOP = Input(UInt(4.W))
-  val aluType = Input(UInt(4.W))
   val out = Output(UInt(srcWidth.W))
 }
 
@@ -21,43 +16,29 @@ class ALUIO_top(srcWidth: Int = 64) extends ALUIO(srcWidth) with ConfigParams {
   val isWordOp = Input(Bool())
 }
 
-class ALU(srcWidth: Int) extends Module with ALUConsts with ConfigParams {
+class ALU(srcWidth: Int) extends Module with ConfigParams {
   val io = IO(new ALUIO(srcWidth))
+  val shamt = if (srcWidth == 64) io.srcB(5, 0).asUInt() else io.srcB(4, 0).asUInt()
+  val pc_4 = io.srcA + 4.U
+  val res_mux = Array(
+    ALU_ADD -> (io.srcA + io.srcB),
+    ALU_SUB -> (io.srcA - io.srcB),
+    ALU_SLT -> (io.srcA.asSInt() < io.srcB.asSInt()),
+    ALU_SLTU -> (io.srcA < io.srcB),
+    ALU_OR -> (io.srcA | io.srcB),
+    ALU_AND -> (io.srcA & io.srcB),
+    ALU_XOR -> (io.srcA ^ io.srcB),
+    ALU_SLL -> (io.srcA << shamt),
+    ALU_SRL -> (io.srcA >> shamt),
+    ALU_SRA -> ((Cat(io.srcA(srcWidth - 1), io.srcA).asSInt() >> shamt) (srcWidth - 1, 0)).asUInt(),
+    ALU_COPY_1 -> io.srcA,
+    ALU_COPY_2 -> io.srcB
+  )
+  io.out := MuxLookup(io.aluOP, io.srcB, res_mux)
 
-  //   ARITHMETIC
-  val sum = io.srcA + Mux(io.aluOP === alu_ADD, io.srcB, -io.srcB)
-
-  // COMPARE
-  val cmp_mux = Array(
-    alu_SLT -> (io.srcA.asSInt() < io.srcB.asSInt()),
-    alu_SLTU -> (io.srcA < io.srcB))
-  val cmp = MuxLookup(io.aluOP, io.srcB, cmp_mux)
-  // LOGIC
-  val logic_mux = Array(
-    alu_OR -> (io.srcA | io.srcB),
-    alu_AND -> (io.srcA & io.srcB),
-    alu_XOR -> (io.srcA ^ io.srcB)
-  )
-  val logic = MuxLookup(io.aluOP, io.srcB, logic_mux)
-  val shamt = if(srcWidth == 64) io.srcB(5, 0).asUInt() else io.srcB(4,0).asUInt()
-  val shift_mux = Array(
-    alu_SLL -> (io.srcA << shamt),
-    alu_SRL -> (io.srcA >> shamt),
-    alu_SRA -> (( Cat(io.srcA(srcWidth-1), io.srcA).asSInt() >> shamt )(srcWidth-1,0)).asUInt()
-  )
-  val shift = MuxLookup(io.aluOP, io.srcB, shift_mux)
-  val copy = Mux(io.aluOP === alu_A, io.srcA, io.srcB)
-  val out_mux = Array(
-    op_Arith -> sum,
-    op_Compare -> cmp,
-    op_Logic -> logic,
-    op_Shift -> shift,
-    op_Copy -> copy
-  )
-  io.out := MuxLookup(io.aluType, io.srcB, out_mux)
 }
 
-class ALU_top extends Module with ALUConsts with ConfigParams {
+class ALU_top extends Module with ConfigParams {
   val io = IO(new ALUIO_top(64))
   val alu32 = Module(new ALU(32))
   val alu64 = Module(new ALU(64))
@@ -66,11 +47,9 @@ class ALU_top extends Module with ALUConsts with ConfigParams {
   alu32.io.srcA := io.srcA
   alu32.io.srcB := io.srcB
   alu32.io.aluOP := io.aluOP
-  alu32.io.aluType := io.aluType
   alu64.io.srcA := io.srcA
   alu64.io.srcB := io.srcB
   alu64.io.aluOP := io.aluOP
-  alu64.io.aluType := io.aluType
 
   // Output selection
   io.out := Mux(io.isWordOp, signExt64(alu32.io.out), alu64.io.out)
