7ca989dd40e01de0edca40d94e525f64cb4514ed
Bohan Hu <bohan.hu@ieee.org>
2020-10-03 15:45:56 +0800

    Add MMIO range

--- src/main/scala/core/mem.scala
+++ src/main/scala/core/mem.scala
@@ -2,6 +2,7 @@ package core
 
 import chisel3._
 import chisel3.stage.ChiselStage
+import chisel3.util.experimental.BoringUtils
 
 class Mem2Wb extends Bundle {
   val aluResult = UInt(64.W)
@@ -15,14 +16,36 @@ class Mem2Wb extends Bundle {
 
 class MEMIO extends Bundle {
   val instBundleIn = Input(new InstBundle)
-  val exe2Mem  =Input(new Exe2Mem)
+  val exe2Mem = Input(new Exe2Mem)
   val mem2Wb = Output(new Mem2Wb)
   val instBundleOut = Output(new InstBundle)
 }
 
+object MMIO {
+  val MMIORange = List(
+    (0x40600000L, 0x10L), // uart
+    (0x50000000L, 0x400000L), // vmem
+    (0x40001000L, 0x8L), // vga ctrl
+    (0x40000000L, 0x1000L), // flash
+    (0x40002000L, 0x1000L), // dummy sdcard
+    (0x42000000L, 0x1000L), // DiffTestCtrl
+    (0x40004000L, 0x1000L), // meipGen
+    (0x40003000L, 0x1000L) // dma
+  )
+
+  def inMMIORange(Addr: UInt) = {
+    MMIORange map ({
+      case (base, size) => Addr >= base.U && Addr <= (base + size).U
+    }) reduce {
+      _ | _
+    }
+  }
+}
+
 class MEM extends Module {
   val io = IO(new MEMIO)
-  val memResult = 0.U(64.W)   // TODO
+  val memResult = 0.U(64.W) // TODO
+
   io.mem2Wb.aluResult := io.exe2Mem.aluResult
   io.mem2Wb.memResult := memResult
   io.mem2Wb.WBSel := io.exe2Mem.WBSel
@@ -32,6 +55,7 @@ class MEM extends Module {
   io.mem2Wb.RdNum := io.exe2Mem.RdNum
   // passthrough
   io.instBundleOut := io.instBundleIn
+  BoringUtils.addSource(RegNext(io.exe2Mem.isMemOp & MMIO.inMMIORange(io.exe2Mem.aluResult)), "difftestIsMMIO")
 }
 
 object MEM extends App {
