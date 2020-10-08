027c63b6c8ef0c9a03027c24b58a1005ddeef35b
Bohan Hu <bohan.hu@ieee.org>
2020-10-08 17:36:30 +0800

    Add support for "C"

--- src/main/scala/core/mdu.scala
+++ src/main/scala/core/mdu.scala
@@ -1,19 +1,22 @@
 package core
 
 import chisel3._
-//
-//class MulIO extends Bundle {
-//  val opA = Input(64.W)
-//  val opB = Input(64.W)
-//  val unsigned = Input(Bool())
-//  val isWordOp = Input(Bool())
-//  val isHi = Input(Bool())
-//  val dWordResult = Output(64.W)
-//  val wordResult = Output(64.W)
-//}
-//
-//
-//
-//class Multiplier extends Module {
-//  val io = IO(new MulIO)
-//}
+
+class MulIO extends Bundle {
+  val opA = Input(UInt(64.W))
+  val opB = Input(UInt(64.W))
+  val unsigned = Input(Bool())
+  val isWordOp = Input(Bool())
+  val isHi = Input(Bool())
+  val dWordResult = Output(UInt(64.W))
+  val wordResult = Output(UInt(64.W))
+}
+
+
+
+class Multiplier extends Module {
+  val io = IO(new MulIO)
+  val mulRes = io.opA * io.opB
+}
+
+
